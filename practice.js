06318d6341411c3c8bf9019ee3d33662555148a0
premjp7777 <jonnalagaddaprem777@gmail.com>
2025-09-06 12:33:33 +0530

    Learned datatypes in js

--- practice.js
+++ practice.js
@@ -1,1 +1,34 @@
 console.log("Hello World, Prem Sagar")
+
+let name = "Dev Astra";
+console.log(name)
+
+let num = 18;
+console.log(num)
+
+let isLogged= false;
+console.log(isLogged)
+
+let x;
+console.log(x)
+
+let y=null;
+
+console.log(y)
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof isLogged)
+console.log(typeof x)
+
+let arr = [1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person = {
+    name:"Prem",
+    age:22
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
